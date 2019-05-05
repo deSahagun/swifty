a880b2061179a2780ccc6b492bac91bbec0c3492
Alex Sahagun <alexsahagun.ortega@gmail.com>
2019-05-04 18:18:28 -0700

    added a hackerrank function

--- functionsAndLoops/myfuncs.playground/Contents.swift
+++ functionsAndLoops/myfuncs.playground/Contents.swift
@@ -8,6 +8,32 @@ func simpleGreeting() -> String{
     return myGreeting
 }
 
+
+func miniMaxSum(arr: [Int]) -> Void {
+    
+    var myArray = arr
+    myArray.sort()
+    
+    // print(myArray)
+    
+    var sumMax : UInt64 = 0
+    var sumMin : UInt64 = 0
+    
+    for index in 1...4{
+        var temp : UInt64 = UInt64(myArray[index])
+        sumMax = sumMax + temp
+        //print(sumMax)
+    }
+    
+    for index in 0...3{
+        var tempTwo : UInt64 = UInt64(myArray[index])
+        sumMin = sumMin + tempTwo
+    }
+    
+    print(String(sumMin) + " " + String(sumMax))
+    
+}
+
 //here is a function with some parameters
 func passMeTwoStrings(first: String, second: String)->Void{
     
