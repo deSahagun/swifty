b6a23e30b3953e615fe94f5fd3bf9fbfbb3d0781
Alex Sahagun <alexsahagun.ortega@gmail.com>
2019-05-04 18:31:32 -0700

    added faster method

--- functionsAndLoops/myfuncs.playground/Contents.swift
+++ functionsAndLoops/myfuncs.playground/Contents.swift
@@ -8,8 +8,8 @@ func simpleGreeting() -> String{
     return myGreeting
 }
 
-
-func miniMaxSum(arr: [Int]) -> Void {
+//O(n) + O(n) and then O(nlogn) to sort average time
+func miniMaxSumTwoLoops(arr: [Int]) -> Void {
     
     var myArray = arr
     myArray.sort()
@@ -33,6 +33,38 @@ func miniMaxSum(arr: [Int]) -> Void {
     print(String(sumMin) + " " + String(sumMax))
     
 }
+
+//O(n) and then O(nlogn) to sort average time
+
+func miniMaxSumOneLoop(arr: [Int]) -> Void {
+    
+    var myArray = arr
+    myArray.sort()
+    
+    var sumMax : UInt64 = 0
+    var sumMin : UInt64 = 0
+    
+    
+    for index in 0...5{
+        if ((index >= 0) && (index < 4))
+        {
+            var tempTwo : UInt64 = UInt64(myArray[index])
+            sumMin = sumMin + tempTwo
+        }
+        
+        if ((index > 0) && (index < 5))
+        {
+            var temp : UInt64 = UInt64(myArray[index])
+            sumMax = sumMax + temp
+        }
+        
+        
+    }
+    
+    print(String(sumMin) + " " + String(sumMax))
+    
+}
+
 
 //here is a function with some parameters
 func passMeTwoStrings(first: String, second: String)->Void{
